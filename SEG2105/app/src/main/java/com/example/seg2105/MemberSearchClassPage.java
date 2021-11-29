e6f42a44fdc2f76a0de50f207a5eb7ae7c708990
Noah Turpin <noahhturpinn@gmail.com>
2021-11-29 17:26:23 -0500

    implementing Search class Page for members?

--- SEG2105/app/src/main/java/com/example/seg2105/MemberSearchClassPage.java
+++ SEG2105/app/src/main/java/com/example/seg2105/MemberSearchClassPage.java
@@ -1,13 +1,75 @@
 package com.example.seg2105;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+
 public class MemberSearchClassPage extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_classname_member);
+
+
+        Context current_context = this;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("calling");
+                try {
+                    loadClassTypes(current_context);
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+
+        View search_button = findViewById(R.id.SearchClass);
+        search_button.setOnClickListener(new View.OnClickListener() { // goes to Search Class Name Page
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MemberSearchClassPage.this, SearchViewPage.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("search_page", "class_type");
+                bundle.putString("search_page_class_type_name", ((Spinner) findViewById(R.id.spinnerClass)).getSelectedItem().toString());
+                intent.putExtras(bundle);
+                MemberSearchClassPage.this.startActivity(intent);
+            }
+        });
+    }
+
+
+    public void loadClassTypes(Context context) throws ExecutionException, InterruptedException {
+
+        ArrayList<ClassType> class_types = ClassType.getAllClassTypes();
+
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                ArrayList<String> class_types_name = new ArrayList<String>();
+                for (ClassType class_type : class_types) {
+                    class_types_name.add(class_type.name);
+                }
+                Spinner spinner = (Spinner) findViewById(R.id.spinnerClass);
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(context, android.R.layout.simple_spinner_item, class_types_name);
+
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+                spinner.setAdapter(adapter);
+            }
+        });
     }
 }
