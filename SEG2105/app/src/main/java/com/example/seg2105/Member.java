ce7e9a47341e9f3d94d569244aa5077ece1c3828
Noah Turpin <noahhturpinn@gmail.com>
2021-11-29 15:10:54 -0500

    Member Class page

--- SEG2105/app/src/main/java/com/example/seg2105/Member.java
+++ SEG2105/app/src/main/java/com/example/seg2105/Member.java
@@ -1,4 +1,20 @@
 package com.example.seg2105;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
 
-public class Member {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Member extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member_page);
+
+        //View viewClass = findViewById(R.id.MemViewClasses);
+
+
+    }
+
 }
